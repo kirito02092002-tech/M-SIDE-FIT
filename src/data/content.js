9cc8e38cfe38f61bac2851bd759cca7d63deb19b
kirito02092002-tech <kirito02092002@gmail.com>
2026-06-27 17:39:15 +0100

    Add files via upload

---
+++ src/data/content.js
@@ -0,0 +1,78 @@
+export const GYM_INFO = {
+  name: 'M Side Fitness',
+  phone: '54 532 411',
+  phoneFull: '(+216) 54 532 411',
+  email: 'msidefitness86@gmail.com',
+  instagram: 'm_side_fitness',
+  instagramUrl: 'https://www.instagram.com/m_side_fitness',
+  facebookUrl: 'https://www.facebook.com/profile.php?id=100057632393402',
+  mapsUrl: 'https://maps.app.goo.gl/SuRN7BffdMCZ2sCf8',
+  coords: { lat: 36.8624, lng: 10.1942 },
+  mapsEmbedUrl:
+    'https://www.google.com/maps?q=M+Side+Fitness,+Ariana+Soghra,+Ariana+2083,+Tunisia&hl=fr&z=16&output=embed',
+};
+
+export const SCHEDULE_KEYS = [
+  'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday',
+];
+
+export const SCHEDULE = {
+  monday: [
+    { time: '18H15', name: 'Kick Boxing', coach: 'Hsan' },
+    { time: '18H15', name: 'U.F.W', coach: 'Oussema' },
+    { time: '19H15', name: 'Spinning', coach: 'Wale' },
+    { time: '19H15', name: 'Body Combat', coach: 'Amine' },
+    { time: '19H30', name: 'F.Training', coach: 'Fedi' },
+    { time: '20H15', name: 'Body Pump', coach: 'Amine' },
+    { time: '20H30', name: 'HIIT', coach: 'Fedi' },
+  ],
+  tuesday: [
+    { time: '12H15', name: 'Just Women', coach: 'Sonia' },
+    { time: '18H15', name: '100% Femme', coach: 'Lamy' },
+    { time: '18H15', name: 'Tae-Bo', coach: 'Wale' },
+    { time: '19H15', name: 'Spartan Challenge', coach: 'Wale & Moatez' },
+    { time: '19H15', name: 'Spinning', coach: 'Nour' },
+    { time: '19H30', name: 'Step', coach: 'Fedi' },
+    { time: '20H30', name: 'Pilates', coach: 'Fedi' },
+  ],
+  wednesday: [
+    { time: '18H15', name: 'Kick Boxing', coach: 'Hsan' },
+    { time: '18H15', name: 'C.A.F', coach: 'Oussema' },
+    { time: '18H15', name: 'Spinning', coach: 'Wale' },
+    { time: '19H15', name: 'U.F.W', coach: 'Oussema' },
+    { time: '19H15', name: 'Pump', coach: 'Wale' },
+    { time: '20H15', name: 'Pencak Silat', coach: 'Slim' },
+  ],
+  thursday: [
+    { time: '12H15', name: 'Just Women', coach: 'Sonia' },
+    { time: '18H15', name: '100% Femme', coach: 'Lamy' },
+    { time: '19H15', name: 'Orientale', coach: 'Islem' },
+    { time: '19H15', name: 'Spinning', coach: 'Nour' },
+    { time: '19H30', name: 'Cardio Mix', coach: 'Fedi' },
+    { time: '20H30', name: 'Pilates', coach: 'Fedi' },
+  ],
+  friday: [
+    { time: '12H15', name: 'Just Women', coach: 'Sonia' },
+    { time: '18H30', name: 'Circuit Training', coach: 'Hsan' },
+    { time: '19H15', name: 'Spinning', coach: 'Walid' },
+    { time: '20H15', name: 'Pencak Silat', coach: 'Slim' },
+  ],
+  saturday: [
+    { time: '15H00', name: 'Karaté Kids', coach: 'Anis' },
+    { time: '16H00', name: 'Gymnastique Kids', coach: 'Ines' },
+  ],
+  sunday: [
+    { time: '10H30', name: 'Karaté Kids', coach: 'Aous' },
+    { time: '14H00', name: 'Gymnastique Kids', coach: 'Ines' },
+    { time: '15H30', name: 'Pencak Silat', coach: 'Slim' },
+  ],
+};
+
+export const SERVICE_ITEMS = [
+  { id: 'group', image: '/images/gym/group-class.jpg' },
+  { id: 'boxing', image: '/images/gym/boxing-class.jpg' },
+  { id: 'cross', image: '/images/gym/instructor-class.jpg' },
+  { id: 'smart', image: '/images/gym/gym-interior-1.jpg' },
+  { id: 'bodypump', image: '/images/gym/bodypump-class.jpg' },
+  { id: 'cardio', image: '/images/gym/cardio.jpg' },
+];
